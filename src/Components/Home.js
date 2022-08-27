cde81a5118481932f214b7cd83186a95f1bbbec5
DanTheManGude <dgude31@gmail.com>
2022-08-27 15:04:33 -0400

    Use typography for Home title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,17 @@
-function Home(props) {
-  return <h1>Home</h1>;
+import Typography from "@mui/material/Typography";
+
+function Home() {
+  return (
+    <Typography
+      variant="h4"
+      sx={{
+        color: "primary.main",
+        textAlign: "center",
+      }}
+    >
+      Home
+    </Typography>
+  );
 }
 
 export default Home;
