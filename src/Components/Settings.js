bf362c114ca43ef26b9909cfabe48b35929cb4bd
DanTheManGude <dgude31@gmail.com>
2022-09-27 21:55:35 -0400

    Render actiosn as cards

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -1,9 +1,82 @@
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 
 function Settings(props) {
   const { addAlert } = props;
+
+  const renderDownloadData = () => {
+    return (
+      <Box sx={{ width: "90%" }}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="h6" color="text.secondary" gutterBottom>
+              Download Data
+            </Typography>
+            <Typography>
+              Download all data that is stored relating to you account.
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ justifyContent: "flex-end" }}>
+            <Button color="info" variant="outlined">
+              Download
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    );
+  };
+
+  const renderImportData = () => {
+    return (
+      <Box sx={{ width: "90%" }}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="h6" color="text.secondary" gutterBottom>
+              Import Data
+            </Typography>
+            <Typography>
+              Import a previosuly dowloaded file of all your data. Note that
+              this will override your current data.
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ justifyContent: "flex-end" }}>
+            <Button color="warning" variant="outlined">
+              Import
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    );
+  };
+
+  const renderDeleteData = () => {
+    return (
+      <Box sx={{ width: "90%" }}>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="h6" color="text.secondary" gutterBottom>
+              Delete Data
+            </Typography>
+            <Typography>
+              Delete all data on your account. We recomend downloading a copy
+              for youself as this action is not undoable.
+            </Typography>
+          </CardContent>
+          <CardActions sx={{ justifyContent: "flex-end" }}>
+            <Button color="error" variant="outlined">
+              Delete
+            </Button>
+          </CardActions>
+        </Card>
+      </Box>
+    );
+  };
+
   return (
     <div>
       <Typography
@@ -17,15 +90,9 @@ function Settings(props) {
         Settings
       </Typography>
       <Stack sx={{ paddingTop: "15px" }} spacing={3} alignItems="center">
-        <Button color="secondary" variant="outlined">
-          Export Data
-        </Button>
-        <Button color="secondary" variant="outlined">
-          Import Data
-        </Button>
-        <Button color="secondary" variant="outlined">
-          Delete Data
-        </Button>
+        {renderDownloadData()}
+        {renderImportData()}
+        {renderDeleteData()}
       </Stack>
     </div>
   );
