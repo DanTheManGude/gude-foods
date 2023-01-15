2ae23eba20536a2b3faa4c01b1a569433a446987
DanTheManGude <dgude31@gmail.com>
2023-01-15 15:19:28 -0500

    Styled dialogue

--- src/Components/Utils/AdvancedFiltersDialogue.js
+++ src/Components/Utils/AdvancedFiltersDialogue.js
@@ -8,6 +8,7 @@ import DialogActions from "@mui/material/DialogActions";
 import Dialog from "@mui/material/Dialog";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 
 import RecipeSearchInput from "../Utils/RecipeSearchInput";
 
@@ -64,66 +65,78 @@ function AdvancedFiltersDialogue(props) {
           <Stack
             key="searchName"
             direction="row"
-            justifyContent="flex-start"
+            justifyContent="space-between"
             alignItems="center"
             spacing={1}
+            sx={{ width: "100%" }}
           >
-            <Typography>Name contains:</Typography>
-            <RecipeSearchInput
-              searchTerm={searchTerm}
-              setSearchTerm={(_searchTerm) => {
-                updateFilteringOptions({ searchTerm: _searchTerm });
-              }}
-              label="Recipe Title"
-            />
+            <Box sx={{ width: "20%" }}>
+              <Typography>Name contains:</Typography>
+            </Box>
+            <Box sx={{ width: "70%" }}>
+              <RecipeSearchInput
+                searchTerm={searchTerm}
+                setSearchTerm={(_searchTerm) => {
+                  updateFilteringOptions({ searchTerm: _searchTerm });
+                }}
+                label="Recipe Title"
+              />
+            </Box>
           </Stack>
           <Stack
             key="ingredientsIncludes"
             direction="row"
-            justifyContent="flex-start"
+            justifyContent="space-between"
             alignItems="center"
             spacing={1}
+            sx={{ width: "100%" }}
           >
-            <Typography>Includes most foods:</Typography>
-            <Autocomplete
-              multiple={true}
-              id="ingredient-input-multi"
-              options={constructBasicFoodOptions(
-                glossary,
-                basicFoodTagOrder || [],
-                unknownSectionName,
-                calculateFoodSectionForOptions
-              )}
-              getOptionLabel={(option) => option.title}
-              groupBy={(option) =>
-                option.foodId
-                  ? calculateFoodSectionForOptions(option.foodId)
-                  : null
-              }
-              isOptionEqualToValue={(optionA, optionB) =>
-                optionA.foodId === optionB.foodId
-              }
-              getOptionDisabled={(option) =>
-                ingredientList && ingredientList.includes(option.foodId)
-              }
-              value={ingredientList.map((foodId) => ({
-                foodId,
-                title: glossary.basicFoods[foodId],
-              }))}
-              onChange={(event, selection) => {
-                const _ingredientList = selection.map(
-                  (option) => option.foodId
-                );
-                updateFilteringOptions({ ingredientList: _ingredientList });
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Ingredients"
-                  placeholder="Enter item"
-                />
-              )}
-            />
+            <Box sx={{ width: "20%" }}>
+              <Typography>Includes foods:</Typography>
+            </Box>
+            <Box sx={{ width: "70%" }}>
+              <Autocomplete
+                multiple={true}
+                limitTags={3}
+                id="ingredient-input-multi"
+                options={constructBasicFoodOptions(
+                  glossary,
+                  basicFoodTagOrder || [],
+                  unknownSectionName,
+                  calculateFoodSectionForOptions
+                )}
+                getOptionLabel={(option) => option.title}
+                groupBy={(option) =>
+                  option.foodId
+                    ? calculateFoodSectionForOptions(option.foodId)
+                    : null
+                }
+                isOptionEqualToValue={(optionA, optionB) =>
+                  optionA.foodId === optionB.foodId
+                }
+                getOptionDisabled={(option) =>
+                  ingredientList && ingredientList.includes(option.foodId)
+                }
+                value={ingredientList.map((foodId) => ({
+                  foodId,
+                  title: glossary.basicFoods[foodId],
+                }))}
+                onChange={(event, selection) => {
+                  const _ingredientList = selection.map(
+                    (option) => option.foodId
+                  );
+                  updateFilteringOptions({ ingredientList: _ingredientList });
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Ingredients"
+                    placeholder="Enter item"
+                  />
+                )}
+                ChipProps={{ color: "secondary", variant: "outlined" }}
+              />
+            </Box>
           </Stack>
         </Stack>
       </DialogContent>
