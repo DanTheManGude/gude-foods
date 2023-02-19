5027f01f94fa31467e64a8a4c64953b97415f921
DanTheManGude <dgude31@gmail.com>
2023-02-19 18:39:00 -0500

    style height

--- src/Components/AppPieces/BottomNav.js
+++ src/Components/AppPieces/BottomNav.js
@@ -5,46 +5,46 @@ import DescriptionSharpIcon from "@mui/icons-material/DescriptionSharp";
 import MenuBookSharpIcon from "@mui/icons-material/MenuBookSharp";
 import SettingsSharpIcon from "@mui/icons-material/SettingsSharp";
 import FormatListBulletedSharpIcon from "@mui/icons-material/FormatListBulletedSharp";
-import Paper from "@mui/material/Paper";
 
 function BottomNav(props) {
   return (
-    <Paper
+    <BottomNavigation
+      showLabels={true}
+      onChange={(event, newValue) => {
+        console.log(newValue);
+      }}
       sx={{
         position: "fixed",
         bottom: 0,
         left: 0,
         right: 0,
+        paddingTop: "10px",
+        height: "80px",
+        alignItems: "flex-start",
       }}
     >
-      <BottomNavigation
-        showLabels
-        onChange={(event, newValue) => {
-          console.log(newValue);
-        }}
-      >
-        <BottomNavigationAction
-          label="Glossary"
-          icon={<DescriptionSharpIcon />}
-        />
-        <BottomNavigationAction
-          label="Shopping"
-          icon={<FormatListBulletedSharpIcon />}
-        />
-        <BottomNavigationAction label="Cookbook" icon={<MenuBookSharpIcon />} />
-        <BottomNavigationAction label="Settings" icon={<SettingsSharpIcon />} />
-        <BottomNavigationAction
-          label="Login"
-          icon={
-            <img
-              width="24px"
-              src="/media/googleLogin/G.png"
-              alt="Login with Google"
-            />
-          }
-        />
-      </BottomNavigation>
-    </Paper>
+      <BottomNavigationAction
+        label="Glossary"
+        icon={<DescriptionSharpIcon />}
+      />
+      <BottomNavigationAction
+        label="Shopping"
+        icon={<FormatListBulletedSharpIcon />}
+      />
+      <BottomNavigationAction label="Cookbook" icon={<MenuBookSharpIcon />} />
+      <BottomNavigationAction label="Settings" icon={<SettingsSharpIcon />} />
+      <BottomNavigationAction
+        label="Login"
+        icon={
+          <img
+            width="24px"
+            src="/media/googleLogin/G.png"
+            alt="Login with Google"
+            style={{ borderRadius: "24px" }}
+          />
+        }
+      />
+    </BottomNavigation>
   );
 }
 
