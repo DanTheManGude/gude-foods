60ca7dc9ba31fea864a3f812effbd11653a95b3f
DanTheManGude <dgude31@gmail.com>
2023-03-01 00:10:53 -0500

    hackAiGeneratedRecipe for testing

--- src/Components/AppPieces/PagesContainer.js
+++ src/Components/AppPieces/PagesContainer.js
@@ -12,13 +12,44 @@ import Glossary from "../Pages/Glossary";
 import Settings from "../Pages/Settings";
 import AiRecipe from "../Pages/AiRecipe";
 
+const hackAiGeneratedRecipe = {
+  name: "Vegetarian Stuffed Peppers",
+  ingredients: [
+    "4 bell peppers, halved and seeded",
+    "1 tablespoon olive oil",
+    "1 onion, diced",
+    "2 cloves garlic, minced",
+    "1 cup cooked quinoa",
+    "1 cup cooked black beans",
+    "1 cup corn",
+    "1 teaspoon chili powder",
+    "1 teaspoon cumin",
+    "1/2 teaspoon smoked paprika",
+    "1/2 teaspoon salt",
+    "1/4 teaspoon black pepper",
+    "1/2 cup shredded cheese",
+  ],
+  instructions: [
+    "Preheat oven to 375°F.",
+    "Place the bell pepper halves in a baking dish.",
+    "Heat the olive oil in a large skillet over medium heat. Add the onion and garlic and cook until softened, about 5 minutes.",
+    "Add the quinoa, black beans, corn, chili powder, cumin, smoked paprika, salt, and black pepper. Cook for an additional 5 minutes, stirring occasionally.",
+    "Remove from heat and stir in the cheese.",
+    "Divide the mixture among the bell pepper halves.",
+    "Bake for 25 minutes, or until the peppers are tender and the cheese is melted.",
+    "Serve warm. Enjoy!",
+  ],
+};
+
 function PagesContainer(props) {
   const { user, addAlert } = props;
 
   const [database, setDatabase] = useState({});
   const [dataPaths, setDataPaths] = useState({});
   const [filteringOptions, setFilteringOptions] = useState();
-  const [aiGeneratedRecipe, setAiGeneratedRecipe] = useState();
+  const [aiGeneratedRecipe, setAiGeneratedRecipe] = useState(
+    hackAiGeneratedRecipe
+  );
   const [openAIKey, setOpenAIKey] = useState();
 
   useEffect(() => {
