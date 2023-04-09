8396b44266c6c4cd13bdbf87b3f44ff41aa7745b
DanTheManGude <dgude31@gmail.com>
2023-04-09 16:53:03 -0400

    Fix looking for when instrustions is included in an instruction

--- src/utils/ai.js
+++ src/utils/ai.js
@@ -20,22 +20,25 @@ export const generateRecipe = (openAIKey, prompt, onSuccess, onFailure) => {
     .catch(onFailure);
 };
 
+const testingText =
+  "\n\nAvocado Pesto Pasta\n\nIngredients:\n\n- 8 ounces of your favorite pasta\n- 2 avocados, pitted and peeled\n- 2 cloves garlic, minced\n- 1/4 cup fresh basil leaves\n- 2 tablespoons freshly squeezed lemon juice\n- 1/4 cup olive oil\n- 1/4 teaspoon salt\n- 1/4 teaspoon freshly ground black pepper\n- 2 tomatoes, diced\n\nInstructions:\n\n1. Bring a large pot of salted water to a boil. Add the pasta and cook according to package instructions.\n\n2. Meanwhile, in a food processor, combine the avocados, garlic, basil, lemon juice, olive oil, salt, and pepper. Process until smooth.\n\n3. Drain the cooked pasta and return it to the pot. Add the avocado pesto and diced tomatoes and stir to combine.\n\n4. Serve the pasta warm or at room temperature. Enjoy!";
+
 export const parseResponse = (textResponse) => {
   const recipe = { name: "", ingredientText: [], instructions: [] };
   let lookingFor = "name";
 
-  const textResponseLines = textResponse.split("\n");
+  const textResponseLines = testingText.split("\n");
   textResponseLines.forEach((line) => {
     if (!line) {
       return;
     }
 
-    if (line.toUpperCase().includes("INSTRUCTIONS")) {
+    if (line.includes("Instructions:")) {
       lookingFor = "instructions";
       return;
     }
 
-    if (line.toUpperCase().includes("INGREDIENTS")) {
+    if (line.includes("Ingredients:")) {
       lookingFor = "ingredients";
       return;
     }
