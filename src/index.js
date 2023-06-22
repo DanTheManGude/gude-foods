816477327faf00c9f6daca680a93505791836d94
DanTheManGude <dgude31@gmail.com>
2023-06-21 23:19:34 -0400

    Proper import

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
 import firebaseConfig from "./firebaseConfig.json";
 
 import "./styles/index.css";
 import { captchaSiteKey } from "./constants";
 import App from "./Components/App";
 
-const {
-  initializeAppCheck,
-  ReCaptchaV3Provider,
-} = require("firebase/app-check");
-
 const firebaseApp = initializeApp(firebaseConfig);
+
 initializeAppCheck(firebaseApp, {
   provider: new ReCaptchaV3Provider(captchaSiteKey),
   isTokenAutoRefreshEnabled: true,
