476b7d7038be5ab7424ecd07944404af932a6dc1
DanTheManGude <dgude31@gmail.com>
2023-07-18 00:59:20 -0400

    make request to openai

--- api/generate-recipe.ts
+++ api/generate-recipe.ts
@@ -2,6 +2,43 @@ export const config = {
   runtime: "edge",
 };
 
-export default (request: Request) => {
-  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
+export default async (request: Request) => {
+  const openAIKey = process.env.OPENAI_KEY;
+
+  const text = await request.text();
+  const data = JSON.parse(text);
+
+  const { prompt } = data;
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${openAIKey}`,
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt,
+      temperature: 0,
+      max_tokens: 800,
+    }),
+  };
+
+  try {
+    const resp = await fetch(
+      "https://api.openai.com/v1/completions",
+      requestOptions
+    ).then((r) => r.json());
+
+    if (resp.choices) {
+      const responseText: string = resp.choices[0].text;
+      return new Response(responseText, { status: 200 });
+    }
+    if (resp.error) {
+      throw resp.error.message;
+    }
+    throw resp;
+  } catch (error) {
+    return new Response(error.toString(), { status: 500 });
+  }
 };
