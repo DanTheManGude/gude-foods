eb416564c76b6c6ad8fab369d39caa858f1c785e
DanTheManGude <dgude31@gmail.com>
2023-07-18 00:12:39 -0400

    create edge function

--- api/generate-recipe.ts
+++ api/generate-recipe.ts
@@ -1,27 +1,7 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { Configuration, OpenAIApi } from "openai";
+export const config = {
+  runtime: "edge",
+};
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-export default async function (
-  request: VercelRequest,
-  response: VercelResponse
-) {
-  try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: request.body.prompt,
-      temperature: 0,
-      max_tokens: 1000,
-    });
-
-    const responseText = completion.data.choices[0].text;
-    response.status(200).send(responseText);
-    return;
-  } catch (error) {
-    response.status(500).send(error.toString());
-  }
-}
+export default (request: Request) => {
+  return new Response(`Hello, from ${request.url} I'm now an Edge Function!`);
+};
