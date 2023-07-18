db8cba671bf4d1cda8132a496e123d27e6d4ac9d
DanTheManGude <dgude31@gmail.com>
2023-07-17 22:41:54 -0400

    use openai package

--- api/generate-recipe.ts
+++ api/generate-recipe.ts
@@ -1,37 +1,27 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { Configuration, OpenAIApi } from "openai";
 
-export default function (request: VercelRequest, response: VercelResponse) {
-  const prompt = request.body.prompt;
-  const openAIKey = process.env.OPEN_AI_KEY;
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${openAIKey}`,
-    },
-    body: JSON.stringify({
+export default async function (
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  try {
+    const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt,
+      prompt: request.body.prompt,
       temperature: 0,
       max_tokens: 1000,
-    }),
-  };
-
-  fetch("https://api.openai.com/v1/completions", requestOptions)
-    .then((r) => r.json())
-    .then((resp) => {
-      if (resp.choices) {
-        const responseText = resp.choices[0].text;
-        response.status(200).send(responseText);
-        return;
-      }
-      if (resp.error) {
-        throw resp.error.message;
-      }
-      throw resp;
-    })
-    .catch((error) => {
-      response.status(500).send(error.toString());
     });
+
+    const responseText = completion.data.choices[0].text;
+    response.status(200).send(responseText);
+    return;
+  } catch (error) {
+    response.status(500).send(error.toString());
+  }
 }
