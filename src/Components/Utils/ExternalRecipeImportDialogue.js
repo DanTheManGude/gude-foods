a27e5bf460f1b8ebf2bdbd0f1a0f771fbc985da5
DanTheManGude <dgude31@gmail.com>
2023-07-18 22:16:58 -0400

    Dialogue contents

--- src/Components/Utils/ExternalRecipeImportDialogue.js
+++ src/Components/Utils/ExternalRecipeImportDialogue.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Typography from "@mui/material/Typography";
@@ -7,6 +8,7 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Dialog from "@mui/material/Dialog";
 import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 
 function NewRecipeDialogue(props) {
   const { open, onClose, setExternalRecipe } = props;
@@ -17,6 +19,8 @@ function NewRecipeDialogue(props) {
     setExternalRecipe(externalRecipe);
     navigate("/externalRecipe");
   };
+
+  const [externalUrl, setExternalUrl] = useState();
 
   const handleImportFromUrl = () => {
     const externalRecipe = {};
@@ -37,13 +41,29 @@ function NewRecipeDialogue(props) {
       >
         <DialogTitle color="primary">Import recipe from URL</DialogTitle>
         <DialogContent dividers={true}>
-          <Stack></Stack>
+          <Stack spacing={2}>
+            <Typography>
+              Paste a URL of a website that features a recipe. The recipe will
+              available for editing before it is saved to your cookbook.
+            </Typography>
+            <TextField
+              size="small"
+              onChange={(event) => {
+                setExternalUrl(event.target.value);
+              }}
+            />
+          </Stack>
         </DialogContent>
         <DialogActions>
           <Button color="secondary" onClick={onClose} variant="contained">
             <Typography>Cancel</Typography>
           </Button>
-          <Button color="primary" onClick={onClose} variant="contained">
+          <Button
+            color="primary"
+            onClick={handleImportFromUrl}
+            variant="contained"
+            disabled={!externalUrl}
+          >
             <Typography>Import</Typography>
           </Button>
         </DialogActions>
