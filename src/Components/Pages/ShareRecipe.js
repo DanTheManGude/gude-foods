96f970af5a9a5bb65df5cab372cbf8fa8e1a3895
DanTheManGude <dgude31@gmail.com>
2023-07-27 23:56:22 -0400

    parse recipe

--- src/Components/Pages/ShareRecipe.js
+++ src/Components/Pages/ShareRecipe.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 
 import Typography from "@mui/material/Typography";
@@ -9,27 +10,52 @@ function ShareRecipe(props) {
 
   let navigate = useNavigate();
   let location = useLocation();
-  const { search } = location;
-  const params = new URLSearchParams(search);
-  const recipeData = params.get("recipeData");
 
-  if (!recipeData) {
-    return (
-      <Stack alignItems="center" spacing={2} sx={{ paddingTop: 2 }}>
-        <Typography variant="h6">
-          It looks like there is no recipe being shared.
-        </Typography>
-        <Button
-          variant="contained"
-          onClick={() => {
-            navigate("/");
-          }}
-        >
-          Go to home page
-        </Button>
-      </Stack>
-    );
+  const [recipe, setRecipe] = useState();
+
+  if (!recipe) {
+    const { search } = location;
+    const params = new URLSearchParams(search);
+    const recipeData = params.get("recipeData");
+    if (!recipeData) {
+      return (
+        <Stack alignItems="center" spacing={2} sx={{ paddingTop: 2 }}>
+          <Typography variant="h6">
+            It looks like there is no recipe being shared.
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Go to home page
+          </Button>
+        </Stack>
+      );
+    }
+
+    const _recipe = JSON.parse(recipeData);
+    setRecipe(_recipe);
+    return null;
   }
+
+  const renderControls = () => {
+    console.log(isAuthorized);
+    return <></>;
+  };
+
+  const renderRecipeBody = () => {
+    console.log(recipe);
+    return <></>;
+  };
+
+  return (
+    <Stack sx={{ paddingTop: 2 }} spacing={1} alignItems="center">
+      {renderControls()}
+      {renderRecipeBody()}
+    </Stack>
+  );
 }
 
 export default ShareRecipe;
