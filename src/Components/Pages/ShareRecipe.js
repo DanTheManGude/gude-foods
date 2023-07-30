8c4a2f190f07fffdfaf62ade7138603780505787
DanTheManGude <dgude31@gmail.com>
2023-07-29 23:25:28 -0400

    Save recipe

--- src/Components/Pages/ShareRecipe.js
+++ src/Components/Pages/ShareRecipe.js
@@ -5,7 +5,15 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
 
-import { UserContext } from "../Contexts";
+import { transformCookbookFromImport } from "../../utils/dataTransfer";
+import { updateFromCookbookImport } from "../../utils/requests";
+
+import {
+  UserContext,
+  DatabaseContext,
+  DataPathsContext,
+  AddAlertContext,
+} from "../Contexts";
 
 function ShareRecipe(props) {
   const { isAuthorized } = props;
@@ -13,6 +21,15 @@ function ShareRecipe(props) {
   let navigate = useNavigate();
   let location = useLocation();
   const user = useContext(UserContext);
+  const addAlert = useContext(AddAlertContext);
+
+  const database = useContext(DatabaseContext);
+  const { glossary: _glossary, recipeOrder: _recipeOrder } = database;
+  const glossary = _glossary || { basicFoods: {}, recipeTags: {} };
+  const recipeOrder = _recipeOrder || [];
+
+  const dataPaths = useContext(DataPathsContext);
+  const { glossaryPath, cookbookPath } = dataPaths;
 
   const [recipe, setRecipe] = useState();
 
@@ -43,10 +60,42 @@ function ShareRecipe(props) {
     return null;
   }
 
+  const handleSave = () => {
+    const transformedData = transformCookbookFromImport(
+      { recipe },
+      glossary,
+      glossaryPath,
+      cookbookPath
+    );
+
+    const updateHandler = (alert) => {
+      navigate(`/recipe/${Object.keys(transformedData.formattedCookbook)[0]}`);
+      addAlert(alert);
+    };
+
+    updateFromCookbookImport(
+      transformedData,
+      dataPaths,
+      recipeOrder,
+      updateHandler
+    );
+  };
+
   const renderControls = () => {
     console.log("isAuthorized:", isAuthorized);
     console.log("user:", user);
-    return <></>;
+    if (isAuthorized) {
+      return (
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={handleSave}
+          sx={{ width: "85%" }}
+        >
+          <Typography>Save recipe to cookbook</Typography>
+        </Button>
+      );
+    }
   };
 
   const renderRecipeBody = () => {
@@ -55,7 +104,7 @@ function ShareRecipe(props) {
   };
 
   return (
-    <Stack sx={{ paddingTop: 2 }} spacing={1} alignItems="center">
+    <Stack sx={{ paddingTop: 2 }} spacing={2} alignItems="center">
       {renderControls()}
       {renderRecipeBody()}
     </Stack>
