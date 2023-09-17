e91a27b88c0ad68c4616d511f58ac3a0c124f71b
DanTheManGude <dgude31@gmail.com>
2023-09-17 17:37:09 -0400

    show alert

--- src/Components/OfflineMode.js
+++ src/Components/OfflineMode.js
@@ -1,5 +1,47 @@
+import { useState } from "react";
+
+import Stack from "@mui/material/Stack";
+import NavBar from "./AppPieces/NavBar";
+import Collapse from "@mui/material/Collapse";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
 function OfflineMode() {
-  return <>{"OfflineMode"}</>;
+  const [showAlert, setShowAlert] = useState(true);
+
+  const renderAlert = () => (
+    <Collapse in={showAlert}>
+      <Alert
+        sx={{ width: { xs: "85%", md: "60%" } }}
+        severity="info"
+        action={
+          <IconButton
+            aria-label="close"
+            color="inherit"
+            size="small"
+            onClick={() => {
+              setShowAlert(false);
+            }}
+          >
+            <CloseIcon fontSize="inherit" />
+          </IconButton>
+        }
+      >
+        <AlertTitle>Offline Mode</AlertTitle>
+        Only viewing recipes that you import is available.
+      </Alert>
+    </Collapse>
+  );
+
+  return (
+    <Stack alignItems="center">
+      <NavBar isAuthorized={false} />
+      {renderAlert()}
+      {"More content"}
+    </Stack>
+  );
 }
 
 export default OfflineMode;
