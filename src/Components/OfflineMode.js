28ffe4e2c07487ce83816fd429cb41d6249f69ac
DanTheManGude <dgude31@gmail.com>
2023-09-17 18:20:08 -0400

    button layouts

--- src/Components/OfflineMode.js
+++ src/Components/OfflineMode.js
@@ -7,14 +7,16 @@ import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
+import { Button, Typography } from "@mui/material";
 
-function OfflineMode() {
+function OfflineMode(props) {
+  const { disableUsingOffline } = props;
+
   const [showAlert, setShowAlert] = useState(true);
 
   const renderAlert = () => (
     <Collapse in={showAlert}>
       <Alert
-        sx={{ width: { xs: "85%", md: "60%" } }}
         severity="info"
         action={
           <IconButton
@@ -35,12 +37,43 @@ function OfflineMode() {
     </Collapse>
   );
 
-  return (
-    <Stack alignItems="center">
-      <NavBar isAuthorized={false} />
-      {renderAlert()}
-      {"More content"}
+  const renderActionButtons = () => (
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      alignItems="center"
+      spacing={2}
+      width={"100%"}
+    >
+      <Button
+        variant="outlined"
+        sx={{ flexGrow: "1" }}
+        onClick={disableUsingOffline}
+      >
+        <Typography>
+          <span>Stop using</span>
+          <br />
+          <span>offline mode</span>
+        </Typography>
+      </Button>
+      <Button variant="contained" sx={{ flexGrow: "1" }}>
+        <Typography>
+          <span>Select recipe</span>
+          <br />
+          <span>to view</span>
+        </Typography>
+      </Button>
     </Stack>
+  );
+
+  return (
+    <>
+      <NavBar isAuthorized={false} />
+      <Stack alignItems="center" spacing={2} width={"100%"}>
+        {renderAlert()}
+        {renderActionButtons()}
+      </Stack>
+    </>
   );
 }
 
