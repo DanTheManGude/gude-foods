b8ba9b22fcb600756ea04a593d400705b6b1c0ba
DanTheManGude <dgude31@gmail.com>
2023-12-27 23:26:03 -0500

    make index ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import { initializeApp } from "firebase/app";
@@ -10,10 +10,10 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "./styles/index.css";
 import App from "./Components/App";
 
-const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE);
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE || "");
 const firebaseApp = initializeApp(firebaseConfig);
 
-const captchaSiteKey = process.env.REACT_APP_CAPTCHA;
+const captchaSiteKey = process.env.REACT_APP_CAPTCHA || "";
 initializeAppCheck(firebaseApp, {
   provider: new ReCaptchaV3Provider(captchaSiteKey),
   isTokenAutoRefreshEnabled: true,
@@ -28,14 +28,18 @@ const notifySubscriber = () => {
   cacheSubscriber();
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App setSubscriber={setSubscriber} />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App setSubscriber={setSubscriber} />
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
 
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register({ onUpdate: notifySubscriber });
