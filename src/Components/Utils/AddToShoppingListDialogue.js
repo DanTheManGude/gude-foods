8ff117443f703582b812f265787b796d65ac8842
Daniel Gude <dgude31@gmail.com>
2024-01-06 14:21:01 -0500

    Menu count when adding (#255)

--- src/Components/Utils/AddToShoppingListDialogue.js
+++ src/Components/Utils/AddToShoppingListDialogue.js
@@ -1,11 +1,16 @@
 import { useContext, useEffect, useState } from "react";
 
+import { styled } from "@mui/material/styles";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import Dialog from "@mui/material/Dialog";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 
 import { addRecipeToShoppingList } from "../../utils/requests";
 
@@ -16,6 +21,17 @@ import {
   DataPathsContext,
   AddAlertContext,
 } from "../Contexts";
+
+const MenuCount = styled(Button)(
+  ({ theme }) => `
+  &&& {
+    &.Mui-disabled {
+      color: ${theme.palette.text.primary};
+      border-color: ${theme.palette.text.primary};
+    }
+  }
+`
+);
 
 function AddToShoppingListDialogue(props) {
   const { open, onClose, recipeId } = props;
@@ -30,21 +46,36 @@ function AddToShoppingListDialogue(props) {
 
   const [ingredients, setIngredients] = useState({});
 
+  const [count, setCount] = useState(1);
+  const incrementCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const decrementCount = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
   useEffect(() => {
     if (cookbook && cookbook[recipeId]) {
       setIngredients(cookbook[recipeId].ingredients);
     }
   }, [cookbook, recipeId]);
 
+  const handleClose = () => {
+    setCount(1);
+
+    onClose();
+  };
+
   const handleAdd = () => {
     addRecipeToShoppingList(
       recipeId,
+      count,
       ingredients,
       { recipeOrder, menu },
       dataPaths,
       addAlert
     );
-    onClose();
+    handleClose();
   };
 
   return (
@@ -66,14 +97,29 @@ function AddToShoppingListDialogue(props) {
           contentsOnly={true}
         />
       </DialogContent>
-
       <DialogActions>
-        <Button color="secondary" onClick={onClose} variant="contained">
-          <Typography>Cancel</Typography>
-        </Button>
-        <Button color="primary" onClick={handleAdd} variant="contained">
-          <Typography>Add</Typography>
-        </Button>
+        <Stack direction="row" spacing={1}>
+          <ButtonGroup variant="outlined" size="small" color="primary">
+            <Button disabled={count === 1} onClick={decrementCount}>
+              <RemoveIcon />
+            </Button>
+            <MenuCount disabled sx={{ color: "primary" }}>
+              <Typography sx={{ fontWeight: "bold" }}>{count}</Typography>
+            </MenuCount>
+            <Button
+              onClick={incrementCount}
+              sx={{ borderLeftColor: "text.primary" }}
+            >
+              <AddIcon />
+            </Button>
+          </ButtonGroup>
+          <Button color="secondary" onClick={handleClose} variant="contained">
+            <Typography>Cancel</Typography>
+          </Button>
+          <Button color="primary" onClick={handleAdd} variant="contained">
+            <Typography>Add</Typography>
+          </Button>
+        </Stack>
       </DialogActions>
     </Dialog>
   );
