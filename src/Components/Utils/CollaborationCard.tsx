86d7b619684e02a9dede54e32d4844ca1c9df666
DanTheManGude <dgude31@gmail.com>
2024-03-23 19:51:53 -0400

    show and copy your own

--- src/Components/Utils/CollaborationCard.tsx
+++ src/Components/Utils/CollaborationCard.tsx
@@ -5,14 +5,18 @@ import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import ContentCopyRoundedIcon from "@mui/icons-material/ContentCopyRounded";
+import IconButton from "@mui/material/IconButton/IconButton";
 
 import {
   AddAlertContext,
   DataPathsContext,
   DatabaseContext,
+  UserContext,
 } from "../Contexts";
 
 function CollaborationCard() {
+  const user = useContext(UserContext);
   const dataPaths = useContext(DataPathsContext);
   const database = useContext(DatabaseContext);
   const addAlert = useContext(AddAlertContext);
@@ -31,9 +35,26 @@ function CollaborationCard() {
           <Stack sx={{ width: "100%" }} spacing={3}>
             <Typography>
               Alow other users to view/ edit your information. Paste their User
-              ID to allow them to view all of your data. You can specift which
-              parts they can edit.
+              ID to allow them to view your data. You can specify which parts
+              they can edit.
             </Typography>
+            <Stack direction={"row"} flexWrap={"wrap"} alignItems={"center"}>
+              <Typography>
+                Share your UID for somone else to give you access. &nbsp;
+              </Typography>
+              <Typography>
+                <code>{user.uid}</code>
+              </Typography>
+              <IconButton
+                size={"small"}
+                color="primary"
+                onClick={() => {
+                  navigator.clipboard.writeText(user.uid);
+                }}
+              >
+                <ContentCopyRoundedIcon />
+              </IconButton>
+            </Stack>
           </Stack>
         </CardContent>
       </Card>
