88b42aba22784d1cce7cf23c31c8807baa8ded46
DanTheManGude <dgude31@gmail.com>
2024-03-30 18:42:12 -0400

    refactor to individual components

--- src/Components/Pages/Collaboration.tsx
+++ src/Components/Pages/Collaboration.tsx
@@ -23,6 +23,94 @@ import {
   UserContext,
 } from "../Contexts";
 
+const RenderedInfoCard = (
+  <Box sx={{ width: "95%" }}>
+    <Card variant="outlined">
+      <CardContent>
+        <Typography>
+          Alow other users to view & edit your account. You can specify which
+          parts they can edit. Also, view & edit the account of those who have
+          gave you access.
+        </Typography>
+      </CardContent>
+    </Card>
+  </Box>
+);
+
+const ShareCard = ({ uid }: { uid: string }) => (
+  <Box sx={{ width: "95%" }}>
+    <Card variant="outlined">
+      <CardContent>
+        <Stack direction={"row"} flexWrap={"wrap"} alignItems={"center"}>
+          <Typography>
+            Share your UID for somone else to give you access. &nbsp;
+          </Typography>
+          <Typography>
+            <code>{uid}</code>
+          </Typography>
+          <IconButton
+            size={"small"}
+            color="primary"
+            onClick={() => {
+              navigator.clipboard.writeText(uid);
+            }}
+          >
+            <ContentCopyRoundedIcon />
+          </IconButton>
+        </Stack>
+      </CardContent>
+    </Card>
+  </Box>
+);
+
+const NewAccessCard = ({
+  onSubmitNewUid,
+}: {
+  onSubmitNewUid: (uid: string) => void;
+}) => {
+  const [newUid, setNewUid] = useState<string>("");
+  const clearNewUid = () => setNewUid("");
+
+  const hanldeNewUidSubmit = () => {
+    onSubmitNewUid(newUid);
+    clearNewUid();
+  };
+
+  return (
+    <Box sx={{ width: "95%" }}>
+      <Card variant="outlined">
+        <CardContent>
+          <Stack spacing={1}>
+            <Typography>
+              Enter someone's UID to give access to your account.
+            </Typography>
+            <Stack direction={"row"} justifyContent="space-between" spacing={1}>
+              <TextField
+                id="collaboration-uid-input"
+                sx={{ flexGrow: 1 }}
+                size="small"
+                label="Enter UID"
+                variant={"outlined"}
+                value={newUid}
+                onChange={(event) => {
+                  setNewUid(event.target.value);
+                }}
+              />
+              <Button
+                variant="outlined"
+                onClick={hanldeNewUidSubmit}
+                disabled={!newUid}
+              >
+                Give access
+              </Button>
+            </Stack>
+          </Stack>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+};
+
 function Collaboration() {
   const user = useContext(UserContext);
   const dataPaths = useContext(DataPathsContext);
@@ -34,11 +122,7 @@ function Collaboration() {
 
   console.log(collaboration);
 
-  const [newUid, setNewUid] = useState<string>("");
-  const clearNewUid = () => setNewUid("");
-
-  const onSubmitNewUid = () => {
-    const givenUid = newUid;
+  const onSubmitNewUid = (newUid: string) => {
     giveReadAccesForCollaboration(
       newUid,
       collaborationPath,
@@ -48,14 +132,14 @@ function Collaboration() {
           message: <Typography>Successfully gave Read access.</Typography>,
           undo: () => {
             revokeAccesForCollaboration(
-              givenUid,
+              newUid,
               collaborationPath,
               () =>
                 addAlert({
                   alertProps: { severity: "success" },
                   message: (
                     <Typography>
-                      Successfully removed access for {givenUid}.
+                      Successfully removed access for {newUid}.
                     </Typography>
                   ),
                 }),
@@ -63,7 +147,6 @@ function Collaboration() {
             );
           },
         });
-        clearNewUid();
       },
       addAlert
     );
@@ -82,76 +165,9 @@ function Collaboration() {
         Collaboration
       </Typography>
       <Stack sx={{ paddingTop: "15px" }} spacing={2} alignItems="center">
-        <Box sx={{ width: "95%" }}>
-          <Card variant="outlined">
-            <CardContent>
-              <Typography>
-                Alow other users to view & edit your account. You can specify
-                which parts they can edit. Also, view & edit the account of
-                those who have gave you access.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Box>
-        <Box sx={{ width: "95%" }}>
-          <Card variant="outlined">
-            <CardContent>
-              <Stack direction={"row"} flexWrap={"wrap"} alignItems={"center"}>
-                <Typography>
-                  Share your UID for somone else to give you access. &nbsp;
-                </Typography>
-                <Typography>
-                  <code>{user.uid}</code>
-                </Typography>
-                <IconButton
-                  size={"small"}
-                  color="primary"
-                  onClick={() => {
-                    navigator.clipboard.writeText(user.uid);
-                  }}
-                >
-                  <ContentCopyRoundedIcon />
-                </IconButton>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Box>
-
-        <Box sx={{ width: "95%" }}>
-          <Card variant="outlined">
-            <CardContent>
-              <Stack spacing={1}>
-                <Typography>
-                  Enter someone's UID to give access to your account.
-                </Typography>
-                <Stack
-                  direction={"row"}
-                  justifyContent="space-between"
-                  spacing={1}
-                >
-                  <TextField
-                    id="collaboration-uid-input"
-                    sx={{ flexGrow: 1 }}
-                    size="small"
-                    label="Enter UID"
-                    variant={"outlined"}
-                    value={newUid}
-                    onChange={(event) => {
-                      setNewUid(event.target.value);
-                    }}
-                  />
-                  <Button
-                    variant="outlined"
-                    onClick={onSubmitNewUid}
-                    disabled={!newUid}
-                  >
-                    Give access
-                  </Button>
-                </Stack>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Box>
+        {RenderedInfoCard}
+        <ShareCard uid={user.uid} />
+        <NewAccessCard onSubmitNewUid={onSubmitNewUid} />
       </Stack>
     </div>
   );
